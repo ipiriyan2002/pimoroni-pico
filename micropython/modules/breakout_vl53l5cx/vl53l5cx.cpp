fade3ca914d005f3753c8ac56849e5ec9e03885e
Piriyan Karu <ipiriyan2002@gmail.com>
2023-12-04 11:04:39 +0000

    Added functionality for reporting target status alongside other results for VL53L5CX in vl53l5cx.cpp

--- micropython/modules/breakout_vl53l5cx/vl53l5cx.cpp
+++ micropython/modules/breakout_vl53l5cx/vl53l5cx.cpp
@@ -236,6 +236,7 @@ mp_obj_t VL53L5CX_get_data(mp_obj_t self_in) {
 
     mp_obj_t tuple_distance_mm[tuple_size];
     mp_obj_t tuple_reflectance[tuple_size];
+    mp_obj_t tuple_target_status[tuple_size];
 
     int32_t average_distance = 0;
     int32_t average_reflectance = 0;
@@ -244,6 +245,7 @@ mp_obj_t VL53L5CX_get_data(mp_obj_t self_in) {
     for(int i = 0u; i < tuple_size; i++) {
         tuple_distance_mm[i] = mp_obj_new_int(results->distance_mm[i]);
         tuple_reflectance[i] = mp_obj_new_int(results->reflectance[i]);
+        tuple_target_status[i] = mp_obj_new_int(results->target_status[i]);
         average_distance += results->distance_mm[i];
         average_reflectance += results->reflectance[i];
     }
@@ -271,10 +273,11 @@ mp_obj_t VL53L5CX_get_data(mp_obj_t self_in) {
         mp_obj_new_attrtuple(tuple_motion_fields, sizeof(tuple_motion) / sizeof(mp_obj_t), tuple_motion), // Motion data
         mp_obj_new_int(tuple_size), // Number of results
         mp_obj_new_tuple(tuple_size, tuple_distance_mm), // Full distance results
-        mp_obj_new_tuple(tuple_size, tuple_reflectance)  // Full reflectange results
+        mp_obj_new_tuple(tuple_size, tuple_reflectance),  // Full reflectange results
+        mp_obj_new_tuple(tuple_size, tuple_target_status) //Target status of each zone
     };
 
-    STATIC const qstr tuple_fields[] = {MP_QSTR_distance_avg, MP_QSTR_reflectance_avg, MP_QSTR_motion_indicator, MP_QSTR_results, MP_QSTR_distance, MP_QSTR_reflectance};
+    STATIC const qstr tuple_fields[] = {MP_QSTR_distance_avg, MP_QSTR_reflectance_avg, MP_QSTR_motion_indicator, MP_QSTR_results, MP_QSTR_distance, MP_QSTR_reflectance, MP_QSTR_target_status};
 
     return mp_obj_new_attrtuple(tuple_fields, sizeof(tuple) / sizeof(mp_obj_t), tuple);
 }
